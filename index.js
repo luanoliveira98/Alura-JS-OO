f75632473cc873fd359c421d591f5d8a32ac303e
Luan <luanoliveira17@hotmail.com>
2021-10-29 18:01:21 -0300

    Class two

--- index.js
+++ index.js
@@ -1,20 +1,39 @@
-class Customer{
+class Customer {
     name;
     cpf;
+}
+
+class CheckingAccount {
     agency;
-    balance;
+    // #balance = 0 https://github.com/tc39/proposal-class-fields#private-fields
+    _balance = 0;
+
+    deposit(value) {
+        if (value <= 0) return;
+        this._balance += value;
+        return value;
+    }
+
+    withdraw(value) {
+        if (this._balance < value || value <= 0) return;
+        this._balance -= value;
+        return value;
+    }
 }
 
 const customer1 = new Customer();
 customer1.name = "Luan";
 customer1.cpf = 11122233309;
-customer1.agency = 1001;
-customer1.balance = 0;
 
 const customer2 = new Customer();
 customer2.name = "Alice";
 customer2.cpf = 88822233309;
-customer2.agency = 1001;
-customer2.balance = 0;
 
-console.log(customer1, customer2);
+const checkingAccountLuan = new CheckingAccount();
+checkingAccountLuan.agency = 1001;
+
+checkingAccountLuan.deposit(200);
+let withdrawnValue = checkingAccountLuan.withdraw(50);
+console.log(withdrawnValue);
+
+console.log(checkingAccountLuan);
